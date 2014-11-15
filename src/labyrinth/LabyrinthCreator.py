83ac1b50ec583b3f5022e47bba062a409de7e0e1
luki96 <fornal96@o2.pl>
2014-11-15 20:01:07 +0100

    Added createLabyrinthEntrance function

--- src/labyrinth/LabyrinthCreator.py
+++ src/labyrinth/LabyrinthCreator.py
@@ -1,9 +1,12 @@
+import random;
+
 LABYRINTH_HEIGHT = 5;
 LABYRINTH_WIDTH = 3;
 LABYRINTH_WALL = 0;
 LABYRINTH_ROAD = 1;
 LABYRINTH_ENTRANCE = 2;
 LABYRINTH_EXIT = 3;
+PROBABILITY = 0.5;
 xEntrancePosition = 0;
 yEntrancePosition = 0;
 xExitPosition = 0;
@@ -16,3 +19,20 @@ def initializeLabyrinth():
     for i in range(LABYRINTH_HEIGHT):
         for j in range(LABYRINTH_WIDTH):
             labyrinthArray[i][j] = LABYRINTH_WALL;
+            
+def createLabyrinthEntrance():
+    global labyrinthArray;
+    global xEntrancePosition;
+    global yEntrancePosition;
+    
+    xEntrancePosition = random.randrange(LABYRINTH_HEIGHT);
+    if ((xEntrancePosition == 0) or (xEntrancePosition == LABYRINTH_HEIGHT - 1)):
+        yEntrancePosition = random.randrange(LABYRINTH_WIDTH);
+        labyrinthArray[xEntrancePosition][yEntrancePosition] = LABYRINTH_ENTRANCE;
+    elif (xEntrancePosition in range(1, LABYRINTH_HEIGHT - 1)):
+        if (random.random() < PROBABILITY):
+            yEntrancePosition = 0;        
+            labyrinthArray[xEntrancePosition][yEntrancePosition] = LABYRINTH_ENTRANCE;
+        else:
+            yEntrancePosition = LABYRINTH_WIDTH - 1;
+            labyrinthArray[xEntrancePosition][yEntrancePosition] = LABYRINTH_ENTRANCE;
