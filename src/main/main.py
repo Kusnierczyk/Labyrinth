50d04b9328c431d62733eac347f0c209b129f3f4
Łukasz Fornalczyk <fornal96@o2.pl>
2014-12-10 12:42:27 +0100

    Update main

--- src/main/main.py
+++ src/main/main.py
@@ -1,8 +1,19 @@
 from labyrinth.LabyrinthCreator import LabyrinthCreator;
+from labyrinth.LabyrinthDisplayer import LabyrinthDisplayer
 
 if __name__ == '__main__':
     labyrinthCreator = LabyrinthCreator();
+    labyrinthDisplayer = LabyrinthDisplayer(labyrinthCreator.LABYRINTH_HEIGHT,
+                                            labyrinthCreator.LABYRINTH_WIDTH);
+    
     labyrinthCreator.initializeLabyrinth();
     labyrinthCreator.createLabyrinthEntrance();
     labyrinthCreator.createLabyrinthExit();
-    labyrinthCreator.show();  
+    labyrinthCreator.show();
+    
+    labyrinthDisplayer.showLabyrinth(labyrinthCreator.labyrinthArray, 
+        labyrinthCreator.LABYRINTH_HEIGHT, labyrinthCreator.LABYRINTH_WIDTH, 
+        labyrinthCreator.LABYRINTH_ENTRANCE, labyrinthCreator.LABYRINTH_EXIT,
+        labyrinthCreator.LABYRINTH_ROAD, labyrinthCreator.LABYRINTH_WALL,
+        labyrinthCreator.xEntrancePosition, labyrinthCreator.yEntrancePosition,
+        labyrinthCreator.xExitPosition, labyrinthCreator.yExitPosition);
