1cbc65fcd49816f0f45dc6d532e4dba899fb55ef
Łukasz Fornalczyk <fornal96@o2.pl>
2014-12-10 11:34:22 +0100

    Added display labyrinth class

---
+++ src/labyrinth/LabyrinthDisplayer.py
@@ -0,0 +1,12 @@
+
+class LabyrinthDisplayer(object):
+    LABYRINTH_ENTRANCE_IMAGE = "entrance.png";
+    LABYRINTH_EXIT_IMAGE = "exit.png";
+    LABYRINTH_BEST_ROAD = "bestRoad.png"
+    LABYRINTH_ROAD_IMAGE = "road.png";
+    LABYRINTH_WALL_IMAGE = "wall.png";
+    IMAGES_FOLDER_NAME = "images";
+
+    def __init__(self, params):
+        pass;
+        
