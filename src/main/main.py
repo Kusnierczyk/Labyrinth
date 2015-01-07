06c4316df04320911b424d72781be593d357a8b8
Łukasz Fornalczyk <fornal96@o2.pl>
2015-01-08 00:38:57 +0100

    Draft main version

--- src/main/main.py
+++ src/main/main.py
@@ -1,19 +1,31 @@
 from labyrinth.base.LabyrinthCreator import LabyrinthCreator;
 from labyrinth.constants.Consts import Consts;
 from labyrinth.display.LabyrinthDisplayer import LabyrinthDisplayer
+from labyrinth.road.RoadFinder import RoadFinder;
 
 if __name__ == '__main__':
+
     labyrinthCreator = LabyrinthCreator();
-    labyrinthDisplayer = LabyrinthDisplayer(Consts.LABYRINTH_HEIGHT,
-                                            Consts.LABYRINTH_WIDTH);
-    
+    roadFinder = RoadFinder();
+    labyrinthDisplayer = LabyrinthDisplayer(Consts.LABYRINTH_HEIGHT, Consts.LABYRINTH_WIDTH);
+
     labyrinthCreator.initializeLabyrinth();
     labyrinthCreator.createLabyrinthEntrance();
     labyrinthCreator.createLabyrinthExit();
     labyrinthCreator.show();
+    labyrinthCreator.createRoadBetweenEntranceAndExit();
+    labyrinthCreator.show();
     
-    labyrinthDisplayer.showLabyrinth(Consts.LABYRINTH_HEIGHT, Consts.LABYRINTH_WIDTH, 
-        Consts.LABYRINTH_ENTRANCE, Consts.LABYRINTH_EXIT, Consts.LABYRINTH_ROAD, 
-        Consts.LABYRINTH_WALL, labyrinthCreator.labyrinthArray,
+    roadFinder.searchRoad(labyrinthCreator.labyrinthArray,labyrinthCreator.xEntrancePosition,
+                          labyrinthCreator.yEntrancePosition, labyrinthCreator.xExitPosition,
+                          labyrinthCreator.yExitPosition);
+    
+    labyrinthCreator.show();    
+    roadFinder.clearFlagsAfterRoadSearch(labyrinthCreator.labyrinthArray, 
+                                         labyrinthCreator.xExitPosition, 
+                                         labyrinthCreator.yExitPosition);
+    labyrinthCreator.show();
+
+    labyrinthDisplayer.showLabyrinth(labyrinthCreator.labyrinthArray,
         labyrinthCreator.xEntrancePosition, labyrinthCreator.yEntrancePosition,
         labyrinthCreator.xExitPosition, labyrinthCreator.yExitPosition);
