fb1a7b6582e2a9de7f4001d23bfb5f8a3f3444de
Łukasz Fornalczyk <fornal96@o2.pl>
2015-01-08 22:05:55 +0100

    Added docs

--- src/labyrinth/constants/Consts.py
+++ src/labyrinth/constants/Consts.py
@@ -1,18 +1,43 @@
 class Consts(object):
     
+    ## Defines labyrinth height
     LABYRINTH_HEIGHT = 5;
+    
+    ## Defines labyrinth height
     LABYRINTH_WIDTH = 5;
+    
+    ## Defines labyrinth wall
     LABYRINTH_WALL = 0;
+    
+    ## Defines labyrinth road
     LABYRINTH_ROAD = 1;
+    
+    ## Defines labyrinth best road
     LABYRINTH_BEST_ROAD = 2
+    
+    ## Defines labyrinth entrance
     LABYRINTH_ENTRANCE = 3;
+    
+    ## Defines labyrinth exit
     LABYRINTH_EXIT = 4;
-    LABYRINTH_ROAD_LEFT = 5;      'komora z nitka w lewo'
-    LABYRINTH_ROAD_RIGHT = 6;     'komora z nitka w prawo'
-    LABYRINTH_ROAD_UP = 7;        'komora z nitka w gore'
-    LABYRINTH_ROAD_DOWN = 8;      'komora z nitka w dol'
+    
+    ## Defines labyrinth road left direction
+    LABYRINTH_ROAD_LEFT = 5;
+    
+    ## Defines labyrinth road right direction
+    LABYRINTH_ROAD_RIGHT = 6;
+    
+    ## Defines labyrinth road up direction
+    LABYRINTH_ROAD_UP = 7;
+    
+    ## Defines labyrinth road down direction
+    LABYRINTH_ROAD_DOWN = 8;
+    
+    ## Defines probability of some events
     PROBABILITY = 0.5;
     
+    ## Consts class constructor
+    # @param self The object pointer 
     def __init__(self):
         pass;
         
