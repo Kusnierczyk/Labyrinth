427d84d35017a6fa3b8f55160e6d1793b10eb140
Łukasz Fornalczyk <fornal96@o2.pl>
2015-01-19 13:55:41 +0100

    Added comments

--- src/labyrinth/validators/ConstsValidator.py
+++ src/labyrinth/validators/ConstsValidator.py
@@ -2,11 +2,16 @@ from labyrinth.constants.Consts import Consts;
 
 class ConstsValidator(object):
     
+    ## Defines minimum labyrinth size
     minimalLabyrinthSize = 3;
     
+    ## ConstsValidator class constructor
+    # @param self The object pointer  
     def __init__(self):
         pass;
     
+    ## Checks if labyrinth size has correct value
+    # @param self The object pointer  
     def checkLabyrinthSize(self):
         if (Consts.LABYRINTH_SIZE < self.minimalLabyrinthSize):
             raise BaseException("The labyrinth size must equal 3 or greater.");
